a1ffd2b04400427c7aeeb7da8c50d5e0f3c5d318
strawb3rry8ear <191830309+strawb3rry8ear@users.noreply.github.com>
2025-06-09 14:35:27 +0800

    Initial commit

---
+++ src/i18n/ui.ts
@@ -0,0 +1,31 @@
+export const languages = {
+  en: "English",
+  zh: "中文",
+};
+
+export const defaultLang = "en";
+
+export const ui = {
+  en: {
+    "site.title": "JU CHUN KO",
+    "nav.home": "Home",
+    "nav.blog": "Blog",
+    "nav.about": "About",
+    "nav.official_website": "Official Website",
+    "nav.transcript": "Transcript",
+    "blog.lastUpdatedOn": "Last updated on",
+    "blog.readIn": "閱讀{lang}版本",
+    "footer.copyright": "© 2025 • JU CHUN KO",
+  },
+  zh: {
+    "site.title": "科技立委葛如鈞．寶博士",
+    "nav.home": "首頁",
+    "nav.blog": "部落格",
+    "nav.about": "關於",
+    "nav.official_website": "官網",
+    "nav.transcript": "逐字稿",
+    "blog.lastUpdatedOn": "最後更新於",
+    "blog.readIn": "Read in {lang}",
+    "footer.copyright": "© 2025 • 科技立委葛如鈞．寶博士",
+  },
+} as const;
