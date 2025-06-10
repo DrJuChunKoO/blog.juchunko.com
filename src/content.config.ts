497253533f8671c7aae2aa060c1c00e92db583c2
strawb3rry8ear <191830309+strawb3rry8ear@users.noreply.github.com>
2025-06-10 12:07:06 +0800

    feat: Add author field and author pages

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,18 +1,34 @@
-import { glob } from 'astro/loaders';
-import { defineCollection, z } from 'astro:content';
+import { defineCollection, z, reference } from "astro:content";
 
 const blog = defineCollection({
-	// Load Markdown and MDX files in the `src/content/blog/` directory.
-	loader: glob({ base: './src/content/blog', pattern: '**/*.{md,mdx}' }),
-	// Type-check frontmatter using a schema
-	schema: ({ image }) => z.object({
-		title: z.string(),
-		description: z.string(),
-		// Transform string to Date object
-		pubDate: z.coerce.date(),
-		updatedDate: z.coerce.date().optional(),
-		heroImage: image().optional(),
-	}),
+  // Type-check frontmatter using a schema
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      description: z.string(),
+      // Transform string to Date object
+      pubDate: z.coerce.date(),
+      updatedDate: z.coerce.date().optional(),
+      heroImage: image().optional(),
+      author: reference("authors"),
+    }),
 });
 
-export const collections = { blog };
+const authors = defineCollection({
+  type: "data",
+  schema: ({ image }) =>
+    z.object({
+      name: z.string(),
+      avatar: image(),
+      bio: z.string(),
+      social: z
+        .object({
+          twitter: z.string().url().optional(),
+          github: z.string().url().optional(),
+          website: z.string().url().optional(),
+        })
+        .optional(),
+    }),
+});
+
+export const collections = { blog, authors };
