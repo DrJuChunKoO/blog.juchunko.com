39fffee99c3b4167f8de1cf708643a8240e46e24
strawb3rry8ear <191830309+strawb3rry8ear@users.noreply.github.com>
2025-06-10 12:09:25 +0800

    feat: Add i18n for author name and bio

--- src/content.config.ts
+++ src/content.config.ts
@@ -18,13 +18,24 @@ const authors = defineCollection({
   type: "data",
   schema: ({ image }) =>
     z.object({
-      name: z.string(),
+      name: z.object({
+        en: z.string(),
+        zh: z.string(),
+      }),
       avatar: image(),
-      bio: z.string(),
+      bio: z.object({
+        en: z.string(),
+        zh: z.string(),
+      }),
       social: z
         .object({
+          email: z.string().email().optional(),
           twitter: z.string().url().optional(),
           github: z.string().url().optional(),
+          facebook: z.string().url().optional(),
+          youtube: z.string().url().optional(),
+          instagram: z.string().url().optional(),
+          threads: z.string().url().optional(),
           website: z.string().url().optional(),
         })
         .optional(),
