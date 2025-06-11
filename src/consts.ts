ccc81c57a971a9d50b23249a6a636343ce119eb9
strawb3rry8ear <191830309+strawb3rry8ear@users.noreply.github.com>
2025-06-11 16:05:56 +0800

    fix: Update SITE_DESCRIPTION to include a detailed introduction in Chinese and adjust language switcher logic for better localization

--- src/consts.ts
+++ src/consts.ts
@@ -1,4 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 export const SITE_TITLE = "Juchunko's Blog";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_DESCRIPTION =
+  "歡迎來到科技立委葛如鈞的部落格，這裡會分享一些科技、政策、社會、環境等議題，也會分享一些生活點滴。";
