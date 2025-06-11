a0d3748da4a7dd9042f752bdb56bdb573209b9ff
strawb3rry8ear <191830309+strawb3rry8ear@users.noreply.github.com>
2025-06-11 12:49:48 +0800

    feat: Integrate KaTeX for math rendering and add social icons to author pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,10 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwindcss from "@tailwindcss/vite";
+// latex
 
+import remarkMath from "remark-math";
+import rehypeKatex from "rehype-katex";
 // https://astro.build/config
 export default defineConfig({
   site: "https://example.com",
@@ -11,4 +14,5 @@ export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+  markdown: { remarkPlugins: [remarkMath], rehypePlugins: [rehypeKatex] },
 });
